1518e3d6b3e3af68e2cf8bacb9656a8bc184039c
Jeff <112958355+flaxss@users.noreply.github.com>
2023-11-03 12:59:35 +0800

    base_url added

--- server/server.js
+++ server/server.js
@@ -10,11 +10,16 @@ dotenv.config({path: '.env'});
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
+// app.use(cors({
+//     origin: [
+//         "http://localhost:3000",
+//         "http://localhost:3001",
+//     ],
+//     credentials: true,
+// }));
+
 app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "http://localhost:3001",
-    ],
+    origin: "*",
     credentials: true,
 }));
 
